5abf4168b1b9fbf26f7a12f0adfdddac0c486315
korimorida <korimorida@gmail.com>
2026-07-04 14:34:49 +0300

    feat: complete login page layout with official logo and styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Login from "@/pages/login"
 
 function App() {
-
   return (
-    <>
-    
-    </>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   )
 }
 
